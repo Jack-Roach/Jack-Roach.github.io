5be2ea8942452c3fa70f5a45da7393d671503c7e
Jack Roach <jsroach@uvm.edu>
2022-10-21 15:12:55 -0400

    Strawberry

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1 style="color:darkblue;">Jack Roach</h1>
+
+<html>
+    <link rel="stylesheet"
+      href="stylesheet.css"
+      type="text/css"
+      media="screen">
+    <head>
+        <title> Look at the strawberry. </title>
+    </head>
+    <body bgcolor="#72C43C"> 
+        <h1> <b>Home</b> </h1>
+        <p lang="en-us"> This is a test sentence </p>
+        //<p>Strawberry<img src="https://i.imgur.com/NMLpcKj.jpeg" alt="Strawberry" title="Strawberry" width="250" height="400"></p>
+    </body>
+</html>
