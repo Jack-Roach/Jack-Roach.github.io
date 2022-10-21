9581a98a787cf303ffb8d136aea21f2f0d0b44fe
Jack Roach <jsroach@uvm.edu>
2022-10-21 19:12:57 -0400

    Attempting to impliment a wee bit of PHP

---
+++ top.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Data Science Portfolio</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Jack Roach">
+        <meta name="description" content="An online portfolio showcasing data science projects by Jack Roach">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keyords" content="jack roach, john roach, jack r, data, data science, data analysis, data scientist, data engineer, portfolio, data science portfolio, developer portfolio, data science projects">
+        <meta name="category" content="Data Science">
+        <meta name="copyright" content="by John Roach">
+        //<meta name="contact" content="">
+
+        <link rel="stylesheet" href="stylesheet.css" type="text/css" media="screen">
+    </head>
+    <?php
+    print '<body id="' . $path_parts['filename'] . '">';
+    ?>
